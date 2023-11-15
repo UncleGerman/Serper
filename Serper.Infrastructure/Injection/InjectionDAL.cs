b6e7391c0e5fb0b08e961911f419b940564429e5
german_ed@ukr.net <german_ed@ukr.net>
2023-11-15 03:04:33 +0200

    Add Angular

---
+++ Serper.Infrastructure/Injection/InjectionDAL.cs
@@ -0,0 +1,39 @@
+﻿using Serper.DAL;
+using Serper.DAL.Repository;
+using Serper.DAL.EntityFramework;
+using Serper.Infrastructure.Repository;
+using Serper.Infrastructure.Repository.Factory;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace Serper.Infrastructure.Injection
+{
+    public static class InjectionDAL
+    {
+        public static IServiceCollection AddDbContext(
+            this IServiceCollection services,
+            IConfiguration configure)
+        {
+            var connection = configure.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationContext>(options =>
+            {
+                options.UseSqlServer(connection);
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddDAL(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IRepositoryFactory, RepositoryFactory>();
+
+            serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            serviceCollection.AddScoped<ISearchRepository, SearchRepository>();
+
+            return serviceCollection;
+        }
+    }
+}
